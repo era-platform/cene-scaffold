18056655dcc29c023fde6191567f425cbcedf72a
Rocketnia <rokitna@hotmail.com>
2022-01-08 23:05:02 -0800

    Change the build destination directory from fin/ to dest/. Modernize the gulpfile a bit. Start depending on `fs-extra`, and stop depending on `when` and `del`. Adjust copyright declarations.

--- README.md
+++ README.md
@@ -10,32 +10,40 @@ In this repo, the directory transformation produces two JavaScript files: One is
 
 This repo also demonstrates how a program can be built from multiple .cene files.
 
+
 ## Setup
 
 To install, you'll need Node.js and npm. This repository also depends on specific npm packages (namely `cene` and `express`), but the `npm install` commands below can take care of those.
 
-To obtain build dependencies and run the directory transformation:
+To obtain build dependencies and run a build:
 
-```
+```bash
 $ npm install
+```
+
+To run a build again:
+
+```bash
 $ npm run build
 ```
 
-This will create a fin/ directory containing the transformation result, as well as an intermediate build/ directory. The fin/ directory itself has dependencies. To obtain server dependencies and start the Node.js server:
+The build will create a dist/ directory, as well as an intermediate build/ directory. For this project, the dist/ directory itself is a project with dependencies of its own. To obtain server dependencies and start the Node.js server:
 
-```
-$ cd fin/
+```bash
+$ cd dist/
 $ npm install
 $ node server.js
 ```
 
 Then you can visit the site served at localhost:8080. The page displays a "Hello, client!" message by constructing some DOM nodes with the JS FFI.
 
+
 ## Why two installations
 
 You may wonder why we're installing additional dependencies after a build step rather than just installing them up front. We're doing this to demonstrate that Cene and its dependencies (npm and Node.js) are only incidental details of Cene's implementation, and they're not necessarily related to the needs of a specific Cene project. For instance, a Cene project could use some other package manager like Maven or RubyGems.
 
 That said, Cene is currently focused on making JavaScript, Node.js, and npm easy to develop for, because that's the kind of development that will help Cene's implementation itself. Some Cene codebases may opt not to be as strict about the language/source/output distinction as this one is.
+
 
 ## About this project
 
--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,51 @@
-var gulp = require( "gulp" );
-var when = require( "when" );
+"use strict";
 
-var spawn = require( "cross-spawn" );
-var del = require( "del" );
+var fse = require("fs-extra");
+var spawn = require("cross-spawn");
+
 
 var ceneLibs = [
     "cene"
 ];
 
-gulp.task( "clean", function () {
-    return del( [ "build", "fin" ] );
-} );
 
-gulp.task( "build", function () {
-    return when.all( [
-        gulp.src( "src/**/*" ).pipe( gulp.dest( "build/src" ) )
-    ].concat(
-        ceneLibs.map( function ( lib ) {
-            return gulp.src(
-                "node_modules/" + lib + "/lib-cene/**/*"
-            ).pipe( gulp.dest( "build/lib/" + lib ) );
-        } )
-    ) ).then( function ( ignored ) {
-        return when.promise( function ( resolve, reject ) {
-            spawn( "npm",
-                "run cene -- build.cene -i build/ -o fin/ -m".split(
-                    " " ),
-            {
-                stdio: [ "ignore", "inherit", "inherit" ]
-            } ).on( "close", function ( code ) {
-                if ( code !== 0 )
+async function runCeneCli(args) {
+    await new Promise((resolve, reject) => {
+        spawn("npm", ["run", "cene", "--"].concat(args), {
+            stdio: ["ignore", "inherit", "inherit"]
+        })
+            .on("close", code => {
+                if (code !== 0)
                     return void reject(
-                        new Error(
-                            "Cene exited with code " + code ) );
+                        new Error(`Cene exited with code ${code}`));
                 
-                resolve( null );
-            } );
-        } );
-    } );
-} );
+                resolve();
+            });
+    });
+}
+
+async function build(opt_options) {
+    var options = Object.assign({
+        minify: true
+    }, opt_options);
+    
+    await fse.ensureDir("build/lib/");
+    await fse.copy("src/", "build/src/");
+    for (const lib of ceneLibs) {
+        await fse.copy(`node_modules/${lib}/lib-cene/`, `build/lib/${lib}/`);
+    }
+    
+    var ceneCliArgs = "build.cene -i build/ -o dist/";
+    if (options.minify)
+        ceneCliArgs += " -m";
+    await runCeneCli(ceneCliArgs.split(" "));
+}
+
+exports.clean = async () => {
+    await fse.remove("build/");
+    await fse.remove("dist/");
+};
+
+exports.build = async () => await build();
+
+exports[ "build-debug" ] = async () => await build({ minify: false });
